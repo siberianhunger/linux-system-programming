f0a5ac9edf356981dc3cde50c2f97182e99138e2
Andrey Leschenko <siberianhunger@gmail.com>
2020-11-16 00:09:29 +0700

    lab 3

--- lab3/main.c
+++ lab3/main.c
@@ -16,32 +16,34 @@ int main(int argc, char *argv[]) {
     int i, pid[argc], status, stat;
     char arg[20];
 // для всех файлов, перечисленных в командной строке
-    if (argc<2) {
+    if (argc < 2) {
         printf("Usage: programm spaces_to_delete textfile1 textfile2  ...\n");
         exit(-1);
     }
-    for (i = 1; i< argc; i++) {
+    for (i = 2; i < argc; i=i+2) {
 // запускаем дочерний процесс
-        strcpy(arg,argv[i]);
+        strcpy(arg, argv[i]);
         pid[i] = fork();
         if (pid[i] == 0) {
 // если выполняется дочерний процесс
 // вызов функции счета количества пробелов в файле
 
-            if (execl("./dynamic.bin","dynamic.bin",argv[i], argv[i + 1], argv[1][0], NULL)<0) {
-                printf("ERROR while start processing file %s\n",argv[i]);
+            if (execl("./2lab.bin", "2lab.bin",  argv[1], argv[i], argv[i + 1],NULL) < 0) {
+                printf("ERROR while start processing file %s\n", argv[i]);
                 exit(-2);
-            }
-            else printf( "processing of file %s started (pid=%d)\n", argv[i],pid[i]);
+            } else {
+                printf("processing of file %s started (pid=%d)\n", argv[i], pid[i]);
+            };
         }
 // если выполняется родительский процесс
     }
     sleep(1);
 // ожидание окончания выполнения всех запущенных процессов
-    for (i = 1; i< argc; i++) {
-        status=waitpid(pid[i],&stat,0);
+    for (i = 1; i < argc; i++) {
+        status = waitpid(pid[i], &stat, 0);
         if (pid[i] == status) {
-            printf("File %s done,  result=%d\n",argv[i],WEXITSTATUS(stat));
+            printf("File %s done,  result=%d\n", argv[i], WEXITSTATUS(stat));
         }
     }
-    return 0; }
+    return 0;
+}
